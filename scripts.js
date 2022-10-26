115a5cda8adc667958b6819bead4d00c56fc32ba
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2022-10-26 14:32:47 -0700

    Adds hover states and dynamic generates alert

--- scripts.js
+++ scripts.js
@@ -2,10 +2,16 @@
 // handle form screen state
 let params = (new URL(document.location)).searchParams;
 let isBillingIssue = params.get('isBillingIssue');
+let isSignedIn = params.get('isSignedIn');
 const billingIssueElems = document.querySelectorAll('[isBillingIssue]')
+const errorMessage = document.querySelector('.error-message');
 
 if (isBillingIssue === 'true') {
     billingIssueElems.forEach((elem) => elem.classList.remove('hidden'))
+}
+
+if (isSignedIn === 'false') {
+    errorMessage.classList.remove('hidden');
 }
 
 
@@ -13,16 +19,27 @@ if (isBillingIssue === 'true') {
 function getData(form) {
     let formData = new FormData(form);
   
-    // maybe print each value/field with for of 
     for (const [key, value] of formData.entries()) {
-        console.log(`${key}, ${value}`);
+        console.log(`${key}: ${value}`);
       }
-    // console.log(Object.fromEntries(formData));
-    console.log(formData)
-}
-  
-document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    getData(event.target);
-  });
 
+    console.log(Object.fromEntries(formData));
+}
+
+const form = document.getElementById("form");
+if (form) {
+    document.getElementById("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        getData(event.target);
+      });
+}
+
+// Responsive Menu
+function toggleResponsive() {
+    let topNav = document.getElementById("topnav");
+    if (topNav.className === "topnav") {
+      topNav.className += " open";
+    } else {
+      topnav.className = "topnav";
+    }
+  } 
