0b9280b09c19faafa9ff591b272f2728e1e7294f
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2022-10-27 10:51:27 -0700

    Cleans up comments

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 // handle form screen state
 let params = (new URL(document.location)).searchParams;
 let isBillingIssue = params.get('isBillingIssue');
@@ -11,77 +10,7 @@ if (isSignedIn === 'false') {
     errorMessage.classList.remove('hidden');
 }
 
-// let billingFragment = new DocumentFragment();
-
-// let billingFields = document.createElement('fieldset');
-// billingFragment.append(billingFields);
-// let legend = document.createElement('legend');
-// legend.innerHTML = "What are the last 4 digits and expiration date of your credit card?*";
-// billingFields.append(legend);
-// let divCC = document.createElement('div');
-// divCC.className = "cc";
-// billingFields.append(divCC);
-// let labelCCDigits = document.createElement('label');
-// labelCCDigits.for = "ccDigits";
-// divCC.append(labelCCDigits);
-// let inputCCDigits = document.createElement('input');
-// inputCCDigits.type = "text";
-// inputCCDigits.pattern = "[0-9]*";
-// inputCCDigits.maxlength = "4";
-// inputCCDigits.id = "ccDigits";
-// inputCCDigits.name = "ccDigits";
-// inputCCDigits.placeholder = "****";
-// inputCCDigits.setAttribute("required", "");
-// divCC.append(inputCCDigits);
-// let divCCExpir = document.createElement('div');
-// divCCExpir.className = "ccExpir";
-// divCC.append(divCCExpir);
-// let labelCCExpirMM = document.createElement('label');
-// labelCCExpirMM.for = "ccExpirMM";
-// divCCExpir.append(labelCCExpirMM);
-// let inputCCExpirMM = document.createElement('input');
-// inputCCExpirMM.type = "tel";
-// inputCCExpirMM.name = "ccExpirMM";
-// inputCCExpirMM.id = "ccExpirMM";
-// inputCCExpirMM.placeholder = "MM";
-// inputCCExpirMM.pattern = "[0-9]*";
-// inputCCExpirMM.maxlength = "2";
-// inputCCExpirMM.setAttribute("required", "");
-// divCCExpir.append(inputCCExpirMM);
-// let expirDivider = document.createElement('div');
-// expirDivider.innerHTML = '/';
-// expirDivider.className = 'border';
-// divCCExpir.append(expirDivider);
-// let labelCCExpirYY = document.createElement('label');
-// labelCCExpirYY.for = "ccExpirYY";
-// divCCExpir.append(labelCCExpirYY);
-// let inputCCExpirYY = document.createElement('input');
-// inputCCExpirYY.type = "tel";
-// inputCCExpirYY.name = "ccExpirYY";
-// inputCCExpirYY.id = "ccExpirYY";
-// inputCCExpirYY.placeholder = "YY";
-// inputCCExpirYY.pattern = "[0-9]*";
-// inputCCExpirYY.maxlength = "2";
-// inputCCExpirYY.setAttribute("required", "");
-// divCCExpir.append(inputCCExpirYY);
-
-// let labelLastCharge = document.createElement('label');
-// labelLastCharge.for = "last-charge";
-// labelLastCharge.innerHTML = "What is the date of your last charge?*";
-// let inputLastCharge = document.createElement('input');
-// billingFragment.append(labelLastCharge);
-// inputLastCharge.type = "date";
-// inputLastCharge.name = "last-charge";
-// inputLastCharge.id = "last-charge";
-// inputLastCharge.setAttribute("required", "");
-// billingFragment.append(inputLastCharge);
-
-// if (isBillingIssue === 'true') {
-//     // attachmentSection.before(billingFragment);
-//     // billingIssueElems.forEach((elem) => elem.classList.remove('hidden'))
-// }
-
-// removes billing fields instead of hiding them, when not appropriate to avoid invalid fields
+// removes billing fields instead of hiding them, to avoid invalid fields
 if (isBillingIssue === 'false') {
   document.querySelectorAll('[isBillingIssue]').forEach((element) => element.remove())
 }
@@ -115,7 +44,6 @@ if (formSubmitButton) {
     }
   })
 }
-
 
 function submitForm(button) {
   button.value = "Received";
