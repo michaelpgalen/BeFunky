8d188a538129b5a4dcd2332d8a523048116943c9
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2022-10-27 10:42:20 -0700

    isBilling fixed to node removal

--- scripts.js
+++ scripts.js
@@ -11,74 +11,78 @@ if (isSignedIn === 'false') {
     errorMessage.classList.remove('hidden');
 }
 
-let billingFragment = new DocumentFragment();
+// let billingFragment = new DocumentFragment();
 
-let billingFields = document.createElement('fieldset');
-billingFragment.append(billingFields);
-let legend = document.createElement('legend');
-legend.innerHTML = "What are the last 4 digits and expiration date of your credit card?*";
-billingFields.append(legend);
-let divCC = document.createElement('div');
-divCC.className = "cc";
-billingFields.append(divCC);
-let labelCCDigits = document.createElement('label');
-labelCCDigits.for = "ccDigits";
-divCC.append(labelCCDigits);
-let inputCCDigits = document.createElement('input');
-inputCCDigits.type = "text";
-inputCCDigits.pattern = "[0-9]*";
-inputCCDigits.maxlength = "4";
-inputCCDigits.id = "ccDigits";
-inputCCDigits.name = "ccDigits";
-inputCCDigits.placeholder = "****";
-inputCCDigits.setAttribute("required", "");
-divCC.append(inputCCDigits);
-let divCCExpir = document.createElement('div');
-divCCExpir.className = "ccExpir";
-divCC.append(divCCExpir);
-let labelCCExpirMM = document.createElement('label');
-labelCCExpirMM.for = "ccExpirMM";
-divCCExpir.append(labelCCExpirMM);
-let inputCCExpirMM = document.createElement('input');
-inputCCExpirMM.type = "tel";
-inputCCExpirMM.name = "ccExpirMM";
-inputCCExpirMM.id = "ccExpirMM";
-inputCCExpirMM.placeholder = "MM";
-inputCCExpirMM.pattern = "[0-9]*";
-inputCCExpirMM.maxlength = "2";
-inputCCExpirMM.setAttribute("required", "");
-divCCExpir.append(inputCCExpirMM);
-let expirDivider = document.createElement('div');
-expirDivider.innerHTML = '/';
-expirDivider.className = 'border';
-divCCExpir.append(expirDivider);
-let labelCCExpirYY = document.createElement('label');
-labelCCExpirYY.for = "ccExpirYY";
-divCCExpir.append(labelCCExpirYY);
-let inputCCExpirYY = document.createElement('input');
-inputCCExpirYY.type = "tel";
-inputCCExpirYY.name = "ccExpirYY";
-inputCCExpirYY.id = "ccExpirYY";
-inputCCExpirYY.placeholder = "YY";
-inputCCExpirYY.pattern = "[0-9]*";
-inputCCExpirYY.maxlength = "2";
-inputCCExpirYY.setAttribute("required", "");
-divCCExpir.append(inputCCExpirYY);
+// let billingFields = document.createElement('fieldset');
+// billingFragment.append(billingFields);
+// let legend = document.createElement('legend');
+// legend.innerHTML = "What are the last 4 digits and expiration date of your credit card?*";
+// billingFields.append(legend);
+// let divCC = document.createElement('div');
+// divCC.className = "cc";
+// billingFields.append(divCC);
+// let labelCCDigits = document.createElement('label');
+// labelCCDigits.for = "ccDigits";
+// divCC.append(labelCCDigits);
+// let inputCCDigits = document.createElement('input');
+// inputCCDigits.type = "text";
+// inputCCDigits.pattern = "[0-9]*";
+// inputCCDigits.maxlength = "4";
+// inputCCDigits.id = "ccDigits";
+// inputCCDigits.name = "ccDigits";
+// inputCCDigits.placeholder = "****";
+// inputCCDigits.setAttribute("required", "");
+// divCC.append(inputCCDigits);
+// let divCCExpir = document.createElement('div');
+// divCCExpir.className = "ccExpir";
+// divCC.append(divCCExpir);
+// let labelCCExpirMM = document.createElement('label');
+// labelCCExpirMM.for = "ccExpirMM";
+// divCCExpir.append(labelCCExpirMM);
+// let inputCCExpirMM = document.createElement('input');
+// inputCCExpirMM.type = "tel";
+// inputCCExpirMM.name = "ccExpirMM";
+// inputCCExpirMM.id = "ccExpirMM";
+// inputCCExpirMM.placeholder = "MM";
+// inputCCExpirMM.pattern = "[0-9]*";
+// inputCCExpirMM.maxlength = "2";
+// inputCCExpirMM.setAttribute("required", "");
+// divCCExpir.append(inputCCExpirMM);
+// let expirDivider = document.createElement('div');
+// expirDivider.innerHTML = '/';
+// expirDivider.className = 'border';
+// divCCExpir.append(expirDivider);
+// let labelCCExpirYY = document.createElement('label');
+// labelCCExpirYY.for = "ccExpirYY";
+// divCCExpir.append(labelCCExpirYY);
+// let inputCCExpirYY = document.createElement('input');
+// inputCCExpirYY.type = "tel";
+// inputCCExpirYY.name = "ccExpirYY";
+// inputCCExpirYY.id = "ccExpirYY";
+// inputCCExpirYY.placeholder = "YY";
+// inputCCExpirYY.pattern = "[0-9]*";
+// inputCCExpirYY.maxlength = "2";
+// inputCCExpirYY.setAttribute("required", "");
+// divCCExpir.append(inputCCExpirYY);
 
-let labelLastCharge = document.createElement('label');
-labelLastCharge.for = "last-charge";
-labelLastCharge.innerHTML = "What is the date of your last charge?*";
-let inputLastCharge = document.createElement('input');
-billingFragment.append(labelLastCharge);
-inputLastCharge.type = "date";
-inputLastCharge.name = "last-charge";
-inputLastCharge.id = "last-charge";
-inputLastCharge.setAttribute("required", "");
-billingFragment.append(inputLastCharge);
+// let labelLastCharge = document.createElement('label');
+// labelLastCharge.for = "last-charge";
+// labelLastCharge.innerHTML = "What is the date of your last charge?*";
+// let inputLastCharge = document.createElement('input');
+// billingFragment.append(labelLastCharge);
+// inputLastCharge.type = "date";
+// inputLastCharge.name = "last-charge";
+// inputLastCharge.id = "last-charge";
+// inputLastCharge.setAttribute("required", "");
+// billingFragment.append(inputLastCharge);
 
-if (isBillingIssue === 'true') {
-    attachmentSection.before(billingFragment);
-    // billingIssueElems.forEach((elem) => elem.classList.remove('hidden'))
+// if (isBillingIssue === 'true') {
+//     // attachmentSection.before(billingFragment);
+//     // billingIssueElems.forEach((elem) => elem.classList.remove('hidden'))
+// }
+
+if (isBillingIssue === 'false') {
+  document.querySelectorAll('[isBillingIssue]').forEach((element) => element.remove())
 }
 
 // form Submit handling
@@ -101,13 +105,16 @@ if (form) {
 }
 
 const formSubmitButton = document.querySelector('input.button');
-formSubmitButton.addEventListener("click", function (event){
-  const invalids = document.querySelectorAll(':invalid');
-  if (invalids.length == 0) {
-    event.target.value = "Received";
-    event.target.style.backgroundColor = 'DarkSeaGreen';
-  }
-})
+if (formSubmitButton) {
+  formSubmitButton.addEventListener("click", function (event){
+    const invalids = document.querySelectorAll(':invalid');
+    if (invalids.length == 0) {
+      event.target.value = "Received";
+      event.target.style.backgroundColor = 'DarkSeaGreen';
+    }
+  })
+}
+
 
 function submitForm(button) {
   button.value = "Received";
